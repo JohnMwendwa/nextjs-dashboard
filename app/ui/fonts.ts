0c054188da1f46ec700f35f83ded56e814e8b310
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-21 20:02:50 +0300

    export a secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
-import {Inter} from "next/font/google";
+import {Inter,Lusitana} from "next/font/google";
 
 export const inter = Inter({subsets:['latin']})
+
+export const lusitana= Lusitana({subsets:["latin"],weight:["400","700"]})
