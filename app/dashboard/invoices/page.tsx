d9f7f1d8f3ac273c75900f0c3f032f3e5b9a2fbc
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-26 10:26:32 +0300

    fetch the total number of pages and pass down as props

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,14 +5,16 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default function InvoicesPage({searchParams}:{searchParams?:{
+export default async function InvoicesPage({searchParams}:{searchParams?:{
   query?:string;
   page?:string;
   }
 }) {
   const query = searchParams?.query || "";
   const currentPage= Number(searchParams?.page || 1)
+  const totalPages = await fetchInvoicesPages(query)
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -26,7 +28,7 @@ export default function InvoicesPage({searchParams}:{searchParams?:{
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
