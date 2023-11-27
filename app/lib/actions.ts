9c223873228f67de0b0e8c6a13aad3464f13f217
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-27 18:55:33 +0300

    create a server action to update invoice details

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema=z.object({
 })
 
 const CreateInvoice=FormSchema.omit({id:true,date:true})
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData:FormData){
     const {customerId,amount,status}= CreateInvoice.parse({
@@ -31,4 +32,23 @@ await sql`
   `;
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
