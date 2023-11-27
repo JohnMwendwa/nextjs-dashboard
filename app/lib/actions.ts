c55209f100a49637e4653768dbc818693e235b9e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-27 19:13:58 +0300

    create a server action for deleting an invoice by id

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -52,3 +52,9 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+// Delete invoice by id
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
