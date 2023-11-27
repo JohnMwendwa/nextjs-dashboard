b09670d5480ed3d365ad78a3181298b189b4ddce
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-27 18:06:51 +0300

    create a server action for creating new invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema=z.object({
+    id:z.string(),
+    customerId:z.string(),
+    amount:z.coerce.number(),
+    status:z.enum(['pending','paid']),
+    date:z.string(),
+})
+
+const CreateInvoice=FormSchema.omit({id:true,date:true})
+
+export async function createInvoice(formData:FormData){
+    const {customerId,amount,status}= CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      });
+
+const amountInCents=amount * 100;
+const date = new Date().toISOString().split('T')[0]
+
+await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices')
+}
