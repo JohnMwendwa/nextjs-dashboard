da413b51ebb49e9b74286e7991d3a8ec0d56c1ce
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-28 00:39:06 +0300

    render not-found page if an invoice is not found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from 'next/navigation';
+
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
@@ -5,6 +7,10 @@ import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
 export default async function EditInvoicePage({params}:{params:{id:string}}) {
     const id = params.id;
  const [invoice,customers] = await Promise.all([fetchInvoiceById(id),fetchCustomers()]);
+
+ if(!invoice){
+  notFound()
+ }
 
   return (
     <main>
