f9a0e15741ca95284f7ddd8675366bc0ae1c12fd
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-27 18:28:16 +0300

    fetch customer and invoice details and pass them down as props

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,24 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
+ 
+export default async function EditInvoicePage({params}:{params:{id:string}}) {
+    const id = params.id;
+ const [invoice,customers] = await Promise.all([fetchInvoiceById(id),fetchCustomers()]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
