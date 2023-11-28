d2f063464c7b83a3bbc0aed6b590bbbaca0dcd9b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-28 10:36:43 +0300

    add accessibility-friendly error messages

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,15 +1,16 @@
 'use client';
 
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { useFormState } from 'react-dom';
+import Link from 'next/link';
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { State, updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,9 +19,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState:State = { message: null, errors: {} };
   const updateInvoiceWithId= updateInvoice.bind(null,invoice.id);
+  const [state,dispatch]=useFormState(updateInvoiceWithId,initialState);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -32,6 +35,7 @@ export default function EditInvoiceForm({
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              aria-labelledby='customer-error'
               defaultValue={invoice.customer_id}
             >
               <option value="" disabled>
@@ -44,6 +48,13 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id='customer-error' aria-live='polite' aria-atomic="true">
+            {state.errors?.customerId && state.errors.customerId.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
           </div>
         </div>
 
@@ -62,9 +73,17 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-labelledby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id='amount-error' aria-live='polite' aria-atomic>
+            {state.errors?.amount && state.errors.amount.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
           </div>
         </div>
 
@@ -74,7 +93,7 @@ export default function EditInvoiceForm({
             Set the invoice status
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
+            <div className="flex gap-4" aria-labelledby='status-error'>
               <div className="flex items-center">
                 <input
                   id="pending"
@@ -109,7 +128,21 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+        <div id='status-error' aria-live='polite' aria-atomic>
+        {state.errors?.status && state.errors.status.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+        </div>
         </fieldset>
+        <div aria-live='polite' aria-atomic>
+          {state.message && (
+              <p className="mt-2 text-sm text-red-500">
+                {state.message}
+              </p>
+            )}
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
