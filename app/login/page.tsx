dae42df7d6ddcefc0422698146a6344343c50964
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-28 13:43:43 +0300

    style form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,9 @@ import LoginForm from '@/app/ui/login-form';
  
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+    <main className="flex items-center justify-center h-screen border-2">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 -mt-16">
+        <div className="flex w-full items-end rounded-lg bg-blue-500 p-3 h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
           </div>
