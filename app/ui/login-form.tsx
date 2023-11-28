17befbb1dfb26f4aadbf81c974edca570305614f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-28 12:54:59 +0300

    add server action to authenticate form data

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,15 +1,21 @@
-import { lusitana } from '@/app/ui/fonts';
+"use client";
+
+import { useFormState, useFormStatus } from 'react-dom';
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
+
 import { Button } from './button';
+import { lusitana } from '@/app/ui/fonts';
+import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
+  const [state, dispatch] = useFormState(authenticate, undefined);
   return (
-    <form className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -58,6 +64,18 @@ export default function LoginForm() {
         <LoginButton />
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
+          <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {state === 'CredentialsSignin' && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">Invalid credentials</p>
+            </>
+          )}
+        </div>
         </div>
       </div>
     </form>
@@ -65,8 +83,10 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
+  const { pending } = useFormStatus();
+ 
   return (
-    <Button className="mt-4 w-full">
+    <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
