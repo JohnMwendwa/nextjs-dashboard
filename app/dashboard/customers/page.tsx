26df0f6b91f9f4bc1e7bf65e787402b711663fc7
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-30 21:10:19 +0300

    fetch customers data and render the customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,22 @@
 import { Metadata } from "next"
+import { Suspense } from "react";
+
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 
 export const metadata:Metadata={
   title:"Customers"
 }
 
-export default function CustomersPage() {
+export default async function CustomersPage({searchParams}:{searchParams:{
+  query?:string;
+  page?:string;
+}}) {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
   return (
-    <div>Customers Page</div>
+    <Suspense >
+        <CustomersTable customers={customers} />
+    </Suspense>
   )
 }
