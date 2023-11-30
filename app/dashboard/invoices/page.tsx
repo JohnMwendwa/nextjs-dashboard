2e08d7bdc722ba6f628bb718165fd5dae4e0c189
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-30 20:32:58 +0300

    add title metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,17 @@
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata:Metadata={
+  title:'Invoices',
+}
 
 export default async function InvoicesPage({searchParams}:{searchParams?:{
   query?:string;
