8fa53c8c925749be60b96f587bb6dfb31fafc019
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-11-30 20:44:06 +0300

    add title metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,13 @@
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers,fetchInvoiceById } from '@/app/lib/data';
+
+export const metadata:Metadata={
+  title:"Edit Invoice"
+}
  
 export default async function EditInvoicePage({params}:{params:{id:string}}) {
     const id = params.id;
